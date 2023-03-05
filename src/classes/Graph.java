8e497c9385c61fd954ac26fc027e8865011f6c24
Robert Pietroiu <96921061+PcGamerRo@users.noreply.github.com>
2023-03-05 19:32:32 +0200

    made the percentage display. needs hashmap sort

--- src/classes/Graph.java
+++ src/classes/Graph.java
@@ -1,10 +1,36 @@
 package classes;
 
-import java.util.HashMap;
+
+import jdk.jfr.Percentage;
+
+import java.util.Map;
 
 public class Graph {
-    public Graph(HashMap<Candidate,Integer> map)
-    {
+    private int[] percentages;
+    private String[] candidates;
+    private Map<Candidate, Integer> map;
+    public Graph(Map<Candidate, Integer> map) {
+        this.map = map; // shallow copy
+        this.percentages = new int[map.size()];
+        this.candidates = new String[map.size()];
+        // TO-DO: implement a deep copy
+    }
 
+    public void Display() {
+        int i=0;
+        for (Map.Entry<Candidate, Integer> e : map.entrySet()) {
+            percentages[i] = e.getValue()*100 / Candidate.totalVotes;
+            candidates[i] = e.getKey().name;
+            i++;
+        }
+        for(int j=0;j<percentages.length;j++) {
+            System.out.format("%s ",candidates[j]);
+            for(int k=1;k<=percentages[j]/10;k++) {
+                System.out.print("|");
+            }
+            System.out.format("     %d ", percentages[j]);
+            System.out.print("%");
+            System.out.println();
+        }
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -3,12 +3,10 @@ package main;
 import classes.*;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("EVENT 1 ||||||||||||| 75%");
-        System.out.println("EVENT 2 ||||| 25%");
-
         Candidate.c_counter = 1;
         Candidate.totalVotes = 0;
         Voter.v_counter = 1;
@@ -17,30 +15,37 @@ public class Main {
         Candidate candidate2 = new Candidate();
         Voter voter1 = new Voter();
         Voter voter2 = new Voter();
+        Voter voter3 = new Voter();
+        Voter voter4 = new Voter();
 
-//        try {
-//            voter1.submit(candidate1);
-//        } catch (InvalidVoteException e) {
-//            throw new RuntimeException(e);
-//        }
-//        try {
-//            voter1.submit(candidate1);
-//        } catch (InvalidVoteException e) {
-//            System.out.println(e.getMessage());
-//        }
-//        try {
-//            voter2.submit(candidate1);
-//        } catch (InvalidVoteException e) {
-//            System.out.println(e.getMessage());
-//        }
+        try {
+            voter1.submit(candidate1);
+        } catch (InvalidVoteException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            voter2.submit(candidate1);
+        } catch (InvalidVoteException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            voter3.submit(candidate2);
+        } catch (InvalidVoteException e) {
+            System.out.println(e.getMessage());
+        }
+        try {
+            voter4.submit(candidate2);
+        } catch (InvalidVoteException e) {
+            System.out.println(e.getMessage());
+        }
 
-        HashMap<Candidate,Integer> map = new HashMap<>();
+        HashMap<Candidate,Integer> map = new HashMap<Candidate,Integer>();
         map.put(candidate1,candidate1.votes);
         map.put(candidate2,candidate2.votes);
-        Graph g = new Graph(map);
 
+        Graph g = new Graph(map);
+        g.Display();
         System.out.println("_________________________");
         System.out.format("Total votes: %d", Candidate.totalVotes);
-
     }
 }
